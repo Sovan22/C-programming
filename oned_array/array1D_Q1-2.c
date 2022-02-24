12c63c4bc5ac012a2f3f68add370b9458a1a1447
Sovan22 <deepsin059@gmail.com>
2022-02-24 16:24:27 +0530

    Update array1D_Q1-2.c

--- oned_array/array1D_Q1-2.c
+++ oned_array/array1D_Q1-2.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main()
 {
-  int array[50];
-  
-  int i,n,x;
+  int oned_array[50];
+   int tSize = 50,uSize = 0;
+   int i,n,r;
+   printf("Enter the number of elements\n");
+   scanf("%d",&uSize);
+    for(i=0; i<uSize; i++) 
+    {
+   	    printf("Enter value for oned_array[%d]:", i);
+        scanf("%d", &oned_array[i]);
+    }
+  int i,n;
     printf("Enter the number of elements to be inserted \n");
     scanf("%d",&n);
     if (n>totalS - uSize)
     {
-        printf("The array is full \n");
+        printf("not enough space\n");
         return -1;
     }
     printf("Enter the elements \n");
@@ -17,4 +25,11 @@ int main()
         scanf("%d",&x);
         array[i]=x;
     }
-}
+  printf("Array elements:\n");
+      //Displaying array elements
+   for(i=0; i<uSize; i++) 
+   {
+    printf("%d ", oned_array[i]);
+   }
+    return 0;
+ }
