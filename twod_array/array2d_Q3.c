a3ce39d1e91634d714b53b2fdc360d0823b89177
Sovan22 <deepsin059@gmail.com>
2022-02-24 15:50:42 +0530

    Add files via upload

---
+++ twod_array/array2d_Q3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main(){
+    int twod_array[50][50];
+    int rows,columns;
+    printf("Enter the number of rows\n");
+    scanf("%d",&rows);
+    printf("Enter the number of columns\n");
+    scanf("%d",&columns);
+    printf("Enter the elements of the array\n");
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<columns;j++)
+        {
+            scanf("%d",&twod_array[i][j]);
+        }
+    }
+
+/* Remove the elements from all the odd numbered position of the array*/
+    for(int i=0; i<rows; i++){
+        int j = 0;
+        if (i%2!=0 && (rows%2!=0 || columns%2!=0))
+            j=1;
+        for(;j<columns;j+=2) {
+            twod_array[i][j] = 0;
+        }
+
+    }
+    //display after removing the elements
+    printf("Two Dimensional array elements after removing the elements:\n");
+    for(int i=0; i<rows; i++){
+        for(int j=0;j<columns;j++) {
+            if(twod_array[i][j]!=0) {
+                printf("%d ", twod_array[i][j]);
+            }
+            else {
+                printf(" ");
+            }
+            if(j==columns-1){
+                printf("\n");
+            }
+        }
+    }
+
+}
